9de3c494f3ed237d0c2209f8617a496c76ce9d0a
mastenka <bittmann.stefan@protonmail.com>
2017-06-11 15:53:08 +0200

    Moved APP to separete file

---
+++ ReactApp/VeryLastChance/src/App.js
@@ -0,0 +1,82 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Camera from 'react-native-camera';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
+
+class BadInstagramCloneApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+      </View>
+    );
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+const VeryLastChance = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: BadInstagramCloneApp },
+});
+
+AppRegistry.registerComponent('VeryLastChance', () => VeryLastChance);
