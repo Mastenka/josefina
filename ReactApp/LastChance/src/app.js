50a9f73f7318137a14e3c7392a5c4dcdcda4a5f2
mastenka <bittmann.stefan@protonmail.com>
2017-06-11 14:56:46 +0200

    Functional commit from VeryLast foto navigation

---
+++ ReactApp/LastChance/src/app.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+
+export default class LastChance extends Component {
+  render() {
+    return (
+       <View style={styles.container}>
+        <Text>
+            Neco
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    width: 50,
+    height: 300,
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+AppRegistry.registerComponent('LastChance', () => LastChance);
